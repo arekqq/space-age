58880402bff68ca472866d797d9c6482ab30e841
Arkadiusz Rogacki <arek.rogacki@gmail.com>
2019-02-05 20:49:48 +0100

    Refactoring- Extracted planet factors

--- SpaceAge.cs
+++ SpaceAge.cs
@@ -3,10 +3,19 @@ using System;
 public class SpaceAge
 {
     private double EarthAge;
+    private const double SecondsInEarthYear_duo = 323; 
+    private static readonly double SecondsInEarthYear = 31557600;
+    private static readonly double MercuryToEarthYears = 0.2408467;
+    private static readonly double VenusToEarthYears = 0.61519726;
+    private static readonly double MarsToEarthYears = 1.8808158;
+    private static readonly double JupiterToEarthYears = 11.862615;
+    private static readonly double SaturnToEarthYears = 29.447498;
+    private static readonly double UranusToEarthYears = 84.016846;
+    private static readonly double NeptuneToEarthYears = 164.79132;
 
     public SpaceAge(int seconds)
     {
-        EarthAge = Convert.ToDouble(seconds) / 31557600;
+        EarthAge = Convert.ToDouble(seconds) / SecondsInEarthYear;
     }
 
     public double OnEarth()
@@ -16,36 +25,36 @@ public class SpaceAge
 
     public double OnMercury()
     {
-        return this.EarthAge / 0.2408467;
+        return this.EarthAge / MercuryToEarthYears;
     }
 
     public double OnVenus()
     {
-        return this.EarthAge / 0.61519726;
+        return this.EarthAge / VenusToEarthYears;
     }
 
     public double OnMars()
     {
-        return this.EarthAge / 1.8808158;
+        return this.EarthAge / MarsToEarthYears;
     }
 
     public double OnJupiter()
     {
-        return this.EarthAge / 11.862615;
+        return this.EarthAge / JupiterToEarthYears;
     }
 
     public double OnSaturn()
     {
-        return this.EarthAge / 29.447498;
+        return this.EarthAge / SaturnToEarthYears;
     }
 
     public double OnUranus()
     {
-        return this.EarthAge / 84.016846;
+        return this.EarthAge / UranusToEarthYears;
     }
 
     public double OnNeptune()
     {
-        return this.EarthAge / 164.79132;
+        return this.EarthAge / NeptuneToEarthYears;
     }
 }
